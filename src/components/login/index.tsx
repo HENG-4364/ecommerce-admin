03af164ebd4a4613dfbf557c5e073a2e89641d91
Lyheng <senglyheng2001@gmail.com>
2023-08-25 15:28:04 +0700

    update

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,43 +1,120 @@
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import {
+  Button,
+  Col,
+  Form,
+  Input,
+  InputGroup,
+  InputGroupText,
+  Row,
+} from 'reactstrap';
+import Swal from 'sweetalert2';
+
 export function Login() {
+  const router = useRouter();
+  const [username,setUsername]=useState("");
+  const [password,setPassword]=useState("");
+  const users = [
+    {
+      usernameInput: 'senglyheng2001@gmail.com',
+      passwordInput: 'Admin@123',
+    },
+  ];
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    console.log('usename', username, 'password', password);
+    let input = { username, password };
+    users.find((user) => {
+      if (
+        user.usernameInput === input.username &&
+        user.passwordInput === input.password
+      ) {
+        localStorage.setItem('login', 'true');
+        router.push('/');
+      }
+      else{
+        Swal.fire({
+          icon: 'error',
+          title: 'Login Failed',
+          text: 'Username Or Password wrong!',
+          footer: '<a href="">Why do I have this issue?</a>',
+          confirmButtonText:"Try Again",
+          confirmButtonColor:"#1e5d86"
+        })
+      }
+    });
+  };
+
   return (
-    <>
-      <div className="form-wrapper rounded" style={{ width: '350px', margin: 'auto',backgroundColor:'#F7F7F7',padding:"50px" }}>
+    <div
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: '100vh', paddingBottom: '200px' }}
+    >
+      <div
+        className="form-wrapper rounded "
+        style={{
+          width: '350px',
+          margin: 'auto',
+          backgroundColor: '#F7F7F7',
+          padding: '50px',
+        }}
+      >
         <div id="logo" className="text-center">
-          <img src="../../assets/media/image/logo-dark.png" alt="image" />
+          <img
+            src=""
+            alt="image"
+            style={{ width: '100px', height: 'auto', marginTop: '15px' }}
+          />
         </div>
 
-        <h5 className="text-center mt-3">Sign in</h5>
+        <h5 className="text-center mt-3">Login</h5>
+        <Form onSubmit={onSubmit}>
+          <Row>
+            <Col md={12}>
+              <InputGroup>
+                <InputGroupText>
+                  <i className="fa-solid fa-at"></i>
+                </InputGroupText>
+                <Input
+                  type="text"
+                  className="form-control"
+                  placeholder="Username or email"
+                  onChange={(e)=>{
+                    setUsername(e.target.value);
+                  }}
+                />
+              </InputGroup>
+            </Col>
+          </Row>
+          <Row className="mt-3">
+            <Col md={12}>
+              <InputGroup>
+                <InputGroupText>
+                  <i className="fa-solid fa-key"></i>
+                </InputGroupText>
+                <Input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  onChange={(e)=>{
+                    setPassword(e.target.value);
+                  }}
+                />
+              </InputGroup>
+            </Col>
+          </Row>
 
-        <form>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Username or email"
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Password"
-            />
-          </div>
           <div className="form-group d-flex justify-content-between">
-            <div className="custom-control custom-checkbox">
-              <input
-                type="checkbox"
-                className="custom-control-input"
-                id="customCheck1"
-              />
-            </div>
             <a href="#">Reset password</a>
           </div>
-          <button className="btn btn-primary btn-block">Sign in</button>
-          <hr />
-          <p className="text-muted">Login with your social media account.</p>
-        </form>
+          <Button className="btn btn-primary btn-block" type="submit">
+            Login
+          </Button>
+        </Form>
+        <hr />
+        <p className="text-muted">Login with your account.</p>
       </div>
-    </>
+    </div>
   );
 }
--- src/screen/Login/index.tsx
+++ src/screen/Login/index.tsx
@@ -1,10 +1,11 @@
-import { Login } from '@/components/login'
-import React from 'react'
+
+import { Login } from '@/components/login';
+import React from 'react';
 
 export default function LoginScreen() {
   return (
     <>
-      <Login/>
+      <Login />
     </>
-  )
+  );
 }
